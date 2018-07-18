afa8918324d42e0382ef4fd7fad11b9bd83cb9c4
Yossef <yuanfu.yang@zhixingglobal.com>
2018-07-18 22:50:54 +0800

    angular first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  HeaderComponent]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) throw new Error('模块已经存在，不能再次加载');
+  }
+}
