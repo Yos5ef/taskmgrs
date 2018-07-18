8a0b5657e72aad8d4f4d2fd4dbf91d5562233b50
Yossef <yuanfu.yang@zhixingglobal.com>
2018-07-18 23:25:03 +0800

    [add] add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
